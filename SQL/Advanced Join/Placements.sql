0ec9b8bf92e5c3a9b1b9da66fb55de6857f511f8
Sanjana Ghosh <64126977+Sanjana-Ghosh@users.noreply.github.com>
2023-07-05 11:37:50 +0530

    Placements.sql

--- SQL/Advanced Join/Placements.sql
+++ SQL/Advanced Join/Placements.sql
@@ -1,4 +1,16 @@
 https://www.hackerrank.com/challenges/placements/problem
+  
+Solution:
+  
+Select S.NAME
+FROM STUDENTS S 
+JOIN FRIENDS F ON S.ID = F.ID
+JOIN PACKAGES P1 ON S.ID = P1.ID
+JOIN PACKAGES P2 ON F.FRIEND_ID = P2.ID
+WHERE P2.SALARY > P1.SALARY
+ORDER BY P2.SALARY;
+
+Alternate Solution:
 
 select Name from
 (select S.ID as own_ID,Name,Salary as own_sal from Packages P join Students S on P.ID = S.ID),
